657eed561ef4b1fe3873d84fc3684988b60ee0ac
lmislm <wszdlm@gmail.com>
2018-02-16 10:37:33 +0800

    finish

--- snow.js
+++ snow.js
@@ -24,24 +24,43 @@ window.onload = function () {
             x: Math.random()*W,
             y: Math.random()*H,
             r: Math.random()*5+2, //min of 2px and max of 7px
-            d: Math.random() * 1 //density of the flake
+            d: Math.random() + 1 //density of the flake
         })
     }
+
     //draw flakes onto canvas
-    function drawflakes() {
+    function drawFlakes() {
         ctx.clearRect(0, 0, W, H);//clear whole screen
         ctx.fillStyle = "white";
         ctx.beginPath();
         for(var i=0; i<mf; i++)
         {
-            var i = flakes[i];
+            var f = flakes[i];
             ctx.moveTo(f.x, f.y);
             ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
         }
-
-
+        ctx.fill();
+        moveFlakes();
     }
 
+    //animate the flakes
+    var angle = 0;
 
+    function moveFlakes() {
+        angle += 0.01;
+        for(var i=0; i< mf; i++){
+            //store current flake
+            var f = flakes[i];
 
+            //update X and Y coordinates of each snowflake
+            f.y += Math.pow(f.d, 2) + 1;
+            f.x += Math.sin(angle) * 2;
+
+            //if the snowflake reaches the bottom, send a new one to the top
+            if(f.y > H){
+                flakes[i] = {x:Math.random()*W, y:0, r:f.r, d:f.d};
+            }
+        }
+    }
+    setInterval(drawFlakes, 25);
 }
